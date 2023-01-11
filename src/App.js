c3be6e2e34a9d8049277e9dc746d9805e70c3841
Sumit-Bhalekar-at-420693339219 <bhalekar.sumit@5d.solutions>
2023-01-11 13:24:21 +0530

    Grid component generic changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import ImportExportIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import * as React from "react";
 import "./App.css";
-import { GridTable } from "./GridTable";
 import { VirtualizedTable } from "./VirtualizedTable";
 
 function App() {
+  // const [rowData, setRowData] = React.useState();
+
+  const simpleEditCellRenderer = () => {
+    return (
+      <div
+        className="d-Washingtonex justify-content-between align-items-center"
+        style={{ cursor: "pointer", width: 70 }}
+      >
+        <EditIcon fontSize="10" /> Edit
+      </div>
+    );
+  };
+
+  const simpleCellRenderer = () => {
+    return (
+      <div
+        className="d-Washingtonex justify-content-between align-items-center"
+        style={{ cursor: "pointer", width: 70 }}
+      >
+        <ImportExportIcon fontSize="10" /> Remove
+      </div>
+    );
+  };
+
   const rows = [
     {
       id: 1,
@@ -53,12 +79,13 @@ function App() {
       age: 16,
     },
   ];
+
   const columns = [
-    // {
-    //   label: "Id",
-    //   dataKey: "id",
-    //   width: 50,
-    // },
+    {
+      label: "Id",
+      dataKey: "id",
+      width: 50,
+    },
     {
       width: 200,
       label: "Street Add1",
@@ -97,12 +124,53 @@ function App() {
     {
       width: 100,
       dataKey: "actions",
+      cellRenderer: simpleEditCellRenderer,
+    },
+    {
+      width: 100,
+      dataKey: "actions",
+      cellRenderer: simpleCellRenderer,
     },
   ];
+
   return (
     <>
-      {/* <GridTable /> */}
-      <VirtualizedTable rowsData={rows} columnsData={columns} />
+      <VirtualizedTable
+        rowsData={rows}
+        columnsData={columns}
+        tableHeight={350}
+        tableWidth={1150}
+        headerHeight={40}
+        rowHeight={40}
+        headerStyle={{
+          fontSize: 14,
+          textTransform: "initial",
+          display: "flex",
+          alignItems: "center",
+        }}
+      />
+      {/* <Button
+        variant="outlined"
+        style={{ marginTop: 20, width: 250 }}
+        startIcon={<AddIcon />}
+        onClick={() => {
+          const addRowData = rowData.concat({
+            id: rowData.length + 1,
+            streetAdd1: "132, My Street, Kingston, New York 12401",
+            streetAdd2: "71 ST. NICHOLAS DRIVE",
+            city: "New York",
+            state: "Washington",
+            zipCode: 32092,
+            effectDate: "10/01/2023",
+            expDate: "11/01/2023",
+            firstName: "Daenerys",
+            age: null,
+          });
+          setRowData(addRowData);
+        }}
+      >
+        Add Service Locations
+      </Button> */}
     </>
   );
 }
--- src/VirtualizedTable/index.js
+++ src/VirtualizedTable/index.js
@@ -1,243 +1,40 @@
-import AddIcon from "@mui/icons-material/AddRounded";
-import EditIcon from "@mui/icons-material/Edit";
-import ImportExportIcon from "@mui/icons-material/Delete";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import * as React from "react";
 import { AutoSizer, Column, Table } from "react-virtualized";
 import "react-virtualized/styles.css";
-import { default as styled } from "styled-components";
 
 export const VirtualizedTable = (props) => {
-  const propsData = JSON.stringify(props);
-  const parsedData = JSON.parse(propsData);
-  const rowFinalData = parsedData.rowsData;
-  const colFinalData = parsedData.columnsData;
-  console.log("ROW PROD DATA----", colFinalData);
+  const rowFinalData = props.rowsData;
+  const colFinalData = props.columnsData;
+  const tableHeight = props.tableHeight;
+  const tableWidth = props.tableWidth;
+  const rowHeight = props.rowHeight;
+  const headerHeight = props.headerHeight;
+  const headerStyle = props.headerStyle;
 
-  const TableStyles = styled.div`
-    .ReactVirtualized__Table__headerRow {
-      display: flex;
-      align-items: center;
-    }
-
-    .ReactVirtualized__Table__row {
-      display: flex;
-      align-items: center;
-    }
-
-    .ReactVirtualized__Table__rowColumn {
-      flex: 1;
-    }
-    .ReactVirtualized__Table__headerTruncatedText {
-      display: inline-block;
-      max-width: 100%;
-      white-space: normal;
-      overflow: visible;
-    }
-  `;
-
-  // const rows = [
-  //   {
-  //     id: 1,
-  //     streetAdd1: "132, My Street, Kingston, New York 12401",
-  //     streetAdd2: "71 ST. NICHOLAS DRIVE",
-  //     city: "New York",
-  //     state: "Washington",
-  //     zipCode: 32092,
-  //     effectDate: "10/01/2023",
-  //     expDate: "11/01/2023",
-  //     firstName: "Jon",
-  //     age: 35,
-  //   },
-  //   {
-  //     id: 2,
-  //     streetAdd1: "132, My Street, Kingston, New York 12401",
-  //     streetAdd2: "71 ST. NICHOLAS DRIVE",
-  //     city: "New York",
-  //     state: "Washington",
-  //     zipCode: 32092,
-  //     effectDate: "10/01/2023",
-  //     expDate: "11/01/2023",
-  //     firstName: "Cersei",
-  //     age: 42,
-  //   },
-  //   {
-  //     id: 3,
-  //     streetAdd1: "132, My Street, Kingston, New York 12401",
-  //     streetAdd2: "71 ST. NICHOLAS DRIVE",
-  //     city: "New York",
-  //     state: "Washington",
-  //     zipCode: 32092,
-  //     effectDate: "10/01/2023",
-  //     expDate: "11/01/2023",
-  //     firstName: "Jaime",
-  //     age: 45,
-  //   },
-  //   {
-  //     id: 4,
-  //     streetAdd1: "132, My Street, Kingston, New York 12401",
-  //     streetAdd2: "71 ST. NICHOLAS DRIVE",
-  //     city: "New York",
-  //     state: "Washington",
-  //     zipCode: 32092,
-  //     effectDate: "10/01/2023",
-  //     expDate: "11/01/2023",
-  //     firstName: "Arya",
-  //     age: 16,
-  //   },
-  // ];
-
-  const [rowData, setRowData] = React.useState(rowFinalData);
-
-  const _cellRenderer = ({ cellData }) => {
-    return rowData.map((dataKey, index) => {
-      // console.log("dataKey-----", dataKey);
-      return (
-        <div style={{ display: "flex" }}>
-          <div style={{ width: 50 }}>{rowData[index].id}</div>
-          <div style={{ width: 200 }}>{rowData[index].streetAdd1}</div>
-          <div style={{ width: 200 }}>{rowData[index].streetAdd2}</div>
-          <div style={{ width: 100 }}>{rowData[index].city}</div>
-          <div style={{ width: 100 }}>{rowData[index].state}</div>
-          <div style={{ width: 100 }}>{rowData[index].zipCode}</div>
-          <div style={{ width: 150 }}>{rowData[index].effectDate}</div>
-          <div style={{ width: 150 }}>{rowData[index].expDate}</div>
-          <div
-            className="d-Washingtonex justify-content-between align-items-center"
-            style={{ cursor: "pointer", width: 70 }}
-          >
-            <EditIcon fontSize="10" /> Edit
-          </div>
-          <div
-            className="d-Washingtonex justify-content-between align-items-center"
-            style={{ cursor: "pointer", marginLeft: 5, width: 70 }}
-          >
-            <ImportExportIcon fontSize="10" /> Delete
-          </div>
-        </div>
-      );
-    });
-  };
-
-  const headerRenderer = ({ label }) => {
-    return (
-      <div style={{ flexDirection: "row", display: "flex" }}>
-        <div className="d-Washingtonex justify-content-between align-items-center">
-          {label}
-        </div>
-      </div>
-    );
-  };
-
-  const columns = [
-    {
-      label: "Id",
-      dataKey: "id",
-      width: 50,
-    },
-    {
-      width: 200,
-      label: "Street Add1",
-      dataKey: "streetAdd1",
-    },
-    {
-      width: 200,
-      label: "Street Add2",
-      dataKey: "streetAdd2",
-    },
-    {
-      width: 100,
-      label: "City",
-      dataKey: "city",
-    },
-    {
-      width: 100,
-      label: "State",
-      dataKey: "state",
-    },
-    {
-      width: 100,
-      label: "Zip Code",
-      dataKey: "zipCode",
-    },
-    {
-      width: 150,
-      label: "Effective date",
-      dataKey: "effectDate",
-    },
-    {
-      width: 150,
-      label: "Expiration date",
-      dataKey: "expDate",
-    },
-    {
-      width: 100,
-      dataKey: "actions",
-    },
-  ];
   return (
-    <Box sx={{ padding: "2%", borderWidth: 1, borderColor: "gray" }}>
-      <AutoSizer style={{ height: "100%" }} disableHeight>
-        {({ height, width }) => (
-          <TableStyles>
-            <Table
-              headerHeight={40}
-              width={1150}
-              height={350}
-              rowHeight={40}
-              rowCount={rowData.length}
-              rowRenderer={(cellData, rowIndex) => _cellRenderer(cellData)}
-              rowGetter={({ index }) => rowData[index]}
-            >
-              {colFinalData.map(({ dataKey, ...other }, index) => {
-                // console.log(dataKey, other);
-                return (
-                  <Column
-                    key={dataKey}
-                    headerStyle={{
-                      fontSize: 14,
-                      textTransform: "initial",
-                      display: "flex",
-                      alignItems: "center",
-                    }}
-                    headerRenderer={(headerProps) =>
-                      headerRenderer({
-                        ...headerProps,
-                        columnIndex: index,
-                      })
-                    }
-                    dataKey={dataKey}
-                    {...other}
-                  />
-                );
-              })}
-            </Table>
-            <Button
-              variant="outlined"
-              style={{ marginTop: 20, width: 250 }}
-              startIcon={<AddIcon />}
-              onClick={() => {
-                const addRowData = rowData.concat({
-                  id: rowData.length + 1,
-                  streetAdd1: "132, My Street, Kingston, New York 12401",
-                  streetAdd2: "71 ST. NICHOLAS DRIVE",
-                  city: "New York",
-                  state: "Washington",
-                  zipCode: 32092,
-                  effectDate: "10/01/2023",
-                  expDate: "11/01/2023",
-                  firstName: "Daenerys",
-                  age: null,
-                });
-                setRowData(addRowData);
-              }}
-            >
-              Add Service Locations
-            </Button>
-          </TableStyles>
-        )}
-      </AutoSizer>
-    </Box>
+    <AutoSizer disableHeight>
+      {({ height, width }) => (
+        <Table
+          headerHeight={headerHeight}
+          width={tableWidth}
+          height={tableHeight}
+          rowHeight={rowHeight}
+          rowCount={rowFinalData.length}
+          rowGetter={({ index }) => rowFinalData[index]}
+        >
+          {colFinalData?.map(({ dataKey, cellRenderer, ...other }, index) => {
+            return (
+              <Column
+                key={dataKey}
+                headerStyle={headerStyle}
+                cellRenderer={cellRenderer}
+                dataKey={dataKey}
+                {...other}
+              />
+            );
+          })}
+        </Table>
+      )}
+    </AutoSizer>
   );
 };
